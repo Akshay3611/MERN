94344c5eec0fe26abcc78c4231d6e1b645ba2b9b
Akshay <akshayt3611@gmail.com>
2025-02-16 12:15:58 +0530

    Arithmetic operators inJS

---
+++ Javascript/03.js
@@ -0,0 +1,14 @@
+// Arithmetic Operators in JS
+
+// Addition
+console.log(2 + 5);
+// Substraction
+console.log(2 - 5);
+// Multiplication
+console.log(2 * 5);
+// Division
+console.log(2 / 5);
+// Modulous
+console.log(2 % 5);
+// Power
+console.log(2 ** 5);
