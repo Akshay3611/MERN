524b876f08d23635f885bcb1d08ac00c73a6ed10
Akshay <akshayt3611@gmail.com>
2025-02-16 12:11:15 +0530

    IMpilicit and EXplicit in JS

---
+++ Javascript/02.js
@@ -0,0 +1,11 @@
+// Implicit
+result = "3" + 2;
+
+console.log(result);
+console.log(typeof result);
+
+// Explicit
+result1 = 3 + "2";
+result1 = Number(result1);
+console.log(result1);
+console.log(typeof result1);
