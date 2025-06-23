cb9816ede5053db5bc47155e99a7efda5a64c054
Akshay <akshayt3611@gmail.com>
2025-06-24 00:50:35 +0530

    variables in JS

--- Javascript/01.js
+++ Javascript/01.js
@@ -1,9 +1,21 @@
 // Variables in JS
-
-let a = 5;
-
+// var is function scoped
+var a = 5;
+// var can be redeclared and reassigned
 console.log(a);
-
-a = "Akshay";
-
+// var is function scoped, meaning it is accessible within the function it is declared in
+// var can be redeclared in the same scope
+var a = 20; // redeclaring var
 console.log(a);
+// let is block scoped
+let b = 10;
+console.log(b);
+// let can be reassigned but not redeclared
+// let cannot be redeclared in the same scope
+// let is block scoped
+const c = 15;
+// const is block scoped and cannot be reassigned
+// const cannot be redeclared in the same scope
+// const is used for constants, meaning the value cannot be changed
+
+console.log(c);
