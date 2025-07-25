bc7307f58e4302a7d8ce8d188f7f7d1ef3d41684
Akshay <akshayt3611@gmail.com>
2025-07-26 00:29:10 +0530

    started JS again

--- Javascript/00.js
+++ Javascript/00.js
@@ -1,3 +1,9 @@
 // Hello this JS tutorial
 console.log("Hello");
 console.log(1 + 2);
+
+let name = "Akshay";
+
+console.log("Hello " + name + "!");
+
+console.log(`Hello ${name}!`); // Template literals for string interpolation
