e6c2e2ca3363904042fb431c26969da69835abae
Akshay <akshayt3611@gmail.com>
2025-07-27 00:35:24 +0530

    callback function in JS

--- Javascript/05.js
+++ Javascript/05.js
@@ -10,3 +10,17 @@ function sayGoodbye() {
 }
 
 greet("Akshay", sayGoodbye);
+
+//callback function using settimer
+
+function delayedgreeting(name, callback) {
+  setTimeout(() => {
+    console.log("Hello i'm delayed" + name);
+  }, 5000);
+}
+
+function test() {
+  console.log("This is a test function");
+}
+delayedgreeting("Akshay", sayGoodbye);
+test();
