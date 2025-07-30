80a10fa8d867353b4eadd319f578e40e93882089
Akshay <akshayt3611@gmail.com>
2025-07-31 01:03:17 +0530

    Array (map , filter, reduce in js)

---
+++ Javascript/09.js
@@ -0,0 +1,32 @@
+// Array (map, filter, reduce)
+
+/* const arr = [10, 29, 39, "apple", false, "banana"];
+
+console.log(arr);
+
+arr.push("orange");
+
+console.log(arr);
+
+arr.unshift(1);
+
+console.log(arr);
+ */
+
+// map function in js
+const arr = [1, 2, 3, 4];
+
+// const doubled = arr.map((n) => n * 2);
+
+// console.log(doubled);
+
+//filter function in js
+
+// const check = arr.filter((n) => n >= 3);
+
+// console.log(check);
+
+// reduce function in js
+
+const sum = arr.reduce((total, n) => total + n);
+console.log(sum);
